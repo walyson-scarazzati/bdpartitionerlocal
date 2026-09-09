756bd3e9836d533534dfbf797adb4fc4ea836e33
Walyson Scarazzati da Silva <walyson.scarazzati@gmail.com>
2026-09-09 15:30:06 +0100

    30. Prática: Particionamento de banco de dados - Introdução e Ajuste dos Steps

--- src/main/java/com/springbatch/bdpartitionerlocal/step/MigrarDadosBancariosStepConfig.java
+++ src/main/java/com/springbatch/bdpartitionerlocal/step/MigrarDadosBancariosStepConfig.java
@@ -1,37 +1,55 @@
 package com.springbatch.bdpartitionerlocal.step;
 
+import com.springbatch.bdpartitionerlocal.dominio.Pessoa;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.partition.support.Partitioner;
+import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemStreamReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.springbatch.bdpartitionerlocal.dominio.DadosBancarios;
 
 @Configuration
 public class MigrarDadosBancariosStepConfig {
-  @Autowired
-  private StepBuilderFactory stepBuilderFactory;
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
 
-  @Autowired
-  @Qualifier("transactionManagerApp")
-  private PlatformTransactionManager transactionManagerApp;
-  
-  @Bean
-  public Step migrarDadosBancariosStep(
-      ItemStreamReader<DadosBancarios> dadosBancariosReader,
-      JdbcBatchItemWriter<DadosBancarios> dadosBancariosWriter) {
-    return stepBuilderFactory
-        .get("migrarDadosBancariosStep")
-        .<DadosBancarios, DadosBancarios>chunk(20000)
-        .reader(dadosBancariosReader)
-        .writer(dadosBancariosWriter)
-        .transactionManager(transactionManagerApp)
-        .stream(dadosBancariosReader)
-        .build();
-  }
+    @Autowired
+    @Qualifier("transactionManagerApp")
+    private PlatformTransactionManager transactionManagerApp;
+
+    @Bean
+    public Step migrarDadosBancariosManager(
+            ItemReader<DadosBancarios> dadosBancariosReader,
+            ItemWriter<DadosBancarios> dadosBancariosWriter,
+            @Qualifier("dadosBancariosPartitioner") Partitioner partitioner,
+            TaskExecutor taskExecutor
+    ) {
+        return stepBuilderFactory.get("migrarDadosBancariosManager")
+                .partitioner("migrarDadosBancarios.manager", partitioner)
+                .taskExecutor(taskExecutor)
+                .gridSize(10)
+                .step(migrarDadosBancariosStep(dadosBancariosReader, dadosBancariosWriter))
+                .build();
+    }
+
+    private Step migrarDadosBancariosStep(
+            ItemReader<DadosBancarios> dadosBancariosReader,
+            ItemWriter<DadosBancarios> dadosBancariosWriter) {
+        return stepBuilderFactory
+                .get("migrarDadosBancariosStep")
+                .<DadosBancarios, DadosBancarios>chunk(2000)
+                .reader(dadosBancariosReader)
+                .writer(dadosBancariosWriter)
+                .transactionManager(transactionManagerApp)
+                .build();
+    }
 }
--- src/main/java/com/springbatch/bdpartitionerlocal/step/MigrarPessoaStepConfig.java
+++ src/main/java/com/springbatch/bdpartitionerlocal/step/MigrarPessoaStepConfig.java
@@ -2,12 +2,15 @@ package com.springbatch.bdpartitionerlocal.step;
 
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import com.springbatch.bdpartitionerlocal.dominio.Pessoa;
@@ -20,14 +23,28 @@ public class MigrarPessoaStepConfig {
   @Autowired
   @Qualifier("transactionManagerApp")
   private PlatformTransactionManager transactionManagerApp;
+    @Bean
+  public Step migrarPessoaManager(
+            ItemReader<Pessoa> pessoaReader,
+            ItemWriter<Pessoa> pessoaWriter,
+            @Qualifier("pessoaPartitioner")Partitioner partitioner,
+            TaskExecutor taskExecutor
+            ){
+      return stepBuilderFactory.get("migrarPessoaManager")
+              .partitioner("migrarPessoa.manager", partitioner)
+              .taskExecutor(taskExecutor)
+              .gridSize(10)
+              .step(migrarPessoaStep(pessoaReader, pessoaWriter))
+              .build();
+  }
 
-  @Bean
-  public Step migrarPessoaStep(
-      ItemReader<Pessoa> pessoaReader, 
-      JdbcBatchItemWriter<Pessoa> pessoaWriter) {
+
+  private Step migrarPessoaStep(
+      ItemReader<Pessoa> pessoaReader,
+      ItemWriter<Pessoa> pessoaWriter) {
     return stepBuilderFactory
         .get("migrarPessoaStep")
-        .<Pessoa, Pessoa>chunk(20000)
+        .<Pessoa, Pessoa>chunk(2000)
         .reader(pessoaReader)
         .writer(pessoaWriter)
         .transactionManager(transactionManagerApp)
