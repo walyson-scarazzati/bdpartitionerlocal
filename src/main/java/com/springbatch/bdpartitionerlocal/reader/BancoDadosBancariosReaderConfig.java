20a046e7ef5c6532c8c7f20f20fb756f25295bfa
Walyson Scarazzati da Silva <walyson.scarazzati@gmail.com>
2026-09-09 16:38:03 +0100

    32. Prática: Particionamento de banco de dados - Customização dos leitores

--- src/main/java/com/springbatch/bdpartitionerlocal/reader/BancoDadosBancariosReaderConfig.java
+++ src/main/java/com/springbatch/bdpartitionerlocal/reader/BancoDadosBancariosReaderConfig.java
@@ -2,24 +2,50 @@ package com.springbatch.bdpartitionerlocal.reader;
 
 import javax.sql.DataSource;
 
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.PagingQueryProvider;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import com.springbatch.bdpartitionerlocal.dominio.DadosBancarios;
+import com.springbatch.bdpartitionerlocal.dominio.Pessoa;
 
 @Configuration
 public class BancoDadosBancariosReaderConfig {
 
+@StepScope
   @Bean
-  public JdbcCursorItemReader<DadosBancarios> dadosBancariosReader(@Qualifier("appDataSource") DataSource dataSource) {
-    return new JdbcCursorItemReaderBuilder<DadosBancarios>()
+  public JdbcPagingItemReader<DadosBancarios> dadosBancariosReader(@Qualifier("appDataSource") DataSource dataSource,
+      @Qualifier("queryProviderDadosBancarios") PagingQueryProvider queryProvider) {
+    return new JdbcPagingItemReaderBuilder<DadosBancarios>()
         .name("dadosBancariosReader")
         .dataSource(dataSource)
-        .sql("select * from dados_bancarios_origem")
-        .beanRowMapper(DadosBancarios.class)
+        .queryProvider(queryProvider)
+        .pageSize(2000)
+        .rowMapper(new BeanPropertyRowMapper<DadosBancarios>(DadosBancarios.class))
         .build();
+  }
+
+  @StepScope
+  @Bean
+  public SqlPagingQueryProviderFactoryBean queryProviderDadosBancarios(
+      @Qualifier("appDataSource") DataSource dataSource,
+      @Value("#{stepExecutionContext['minValue']}") Long minValue,
+      @Value("#{stepExecutionContext['maxValue']}") Long maxValue) {
+    SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+    queryProvider.setSelectClause("*");
+    queryProvider.setFromClause("from dados_bancarios_origem");
+    queryProvider.setWhereClause("where id >= " + minValue + " and " + "id <= " + maxValue);
+    queryProvider.setSortKey("id");
+    queryProvider.setDataSource(dataSource);
+    return queryProvider;
   }
 }
--- src/main/java/com/springbatch/bdpartitionerlocal/reader/BancoPessoaReaderConfig.java
+++ src/main/java/com/springbatch/bdpartitionerlocal/reader/BancoPessoaReaderConfig.java
@@ -2,24 +2,47 @@ package com.springbatch.bdpartitionerlocal.reader;
 
 import javax.sql.DataSource;
 
-import org.springframework.batch.item.database.JdbcCursorItemReader;
-import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import com.springbatch.bdpartitionerlocal.dominio.Pessoa;
 
 @Configuration
 public class BancoPessoaReaderConfig {
-  
+
+  @StepScope
   @Bean
-  public JdbcCursorItemReader<Pessoa> pessoaReader(@Qualifier("appDataSource") DataSource dataSource) {
-    return new JdbcCursorItemReaderBuilder<Pessoa>()
+  public JdbcPagingItemReader<Pessoa> pessoaReader(@Qualifier("appDataSource") DataSource dataSource,
+      @Qualifier("queryProviderPessoa") PagingQueryProvider queryProvider) {
+    return new JdbcPagingItemReaderBuilder<Pessoa>()
         .name("pessoaReader")
         .dataSource(dataSource)
-        .sql("select * from dados_bancarios_origem")
-        .beanRowMapper(Pessoa.class)
+        .queryProvider(queryProvider)
+        .pageSize(2000)
+        .rowMapper(new BeanPropertyRowMapper<Pessoa>(Pessoa.class))
         .build();
+  }
+
+  @StepScope
+  @Bean
+  public SqlPagingQueryProviderFactoryBean queryProviderPessoa(
+      @Qualifier("appDataSource") DataSource dataSource,
+      @Value("#{stepExecutionContext['minValue']}") Long minValue,
+      @Value("#{stepExecutionContext['maxValue']}") Long maxValue) {
+    SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+    queryProvider.setSelectClause("*");
+    queryProvider.setFromClause("from pessoa_origem");
+    queryProvider.setWhereClause("where id >= " + minValue + " and " + "id <= " + maxValue);
+    queryProvider.setSortKey("id");
+    queryProvider.setDataSource(dataSource);
+    return queryProvider;
   }
 }
